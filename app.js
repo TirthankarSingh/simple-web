96e25d699cfdfd65d4df204a5a5192cac2bc02ca
TirthankarSingh <tirthankar.qa@gmail.com>
2025-08-12 23:40:57 +0530

    js app removed

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-  res.send(`Hello from Kubernetes! Version: ${process.env.APP_VERSION || 'v1'}`);
-});
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
